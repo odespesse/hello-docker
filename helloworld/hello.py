1988d44c100fa9883f074dc3bcf56c2a2aa20fab
Olivier Despesse <olived19@gmail.com>
2016-03-18 19:10:36 +0100

    Add a debug parameter to the helloworld webapp in order to better analyze what is going on.
    - The parameter is "debug=1" and should be used like "http://127.0.0.1/greetings?debug=1".
    - It will display the current date when the request is received, the hostname and the URL.

--- helloworld/hello.py
+++ helloworld/hello.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask
+import os, socket, datetime
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/greetings")
@@ -12,7 +12,13 @@ def hello():
     count += 1
     with open(count_path, 'w') as f:
         f.write(str(count))
-    return "Hello World! {0}".format(count)
+    res = "Hellow World! {0}".format(count)
+    if 'debug' in request.args and request.args['debug'] == '1':
+        hostname = socket.gethostname()
+        now =  datetime.datetime.now()
+        res += "<br/><br/>Datetime : {0}<br/>Hostname : {1}".format(now, hostname)
+        res += "<br/>Request : {0} {1}".format(request.method, request.url)
+    return res
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=80)
